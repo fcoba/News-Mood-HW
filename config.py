6cc5bbda2570bac923e36616c3822a45df7a99a9
Filis Coba <filiscoba1@gmail.com>
2018-07-13 18:08:24 -0400

    Social Media Hw

---
+++ config.py
@@ -0,0 +1,6 @@
+# These keys are not valid. Use your own API keys
+
+consumer_key = "zyntoJgQ6fRtREVw30MUZ6Zcz"
+consumer_secret = "PwIIsOxZxTxXFGkols5ZUgUZhpFeGamGwCt2cmei4njcIERyMa"
+access_token = "1013068590898085888-rMSriKkqknILIis9YxENojF21YD3Wr"
+access_token_secret = "tDbEHDmkfoCtemHCtBcmMB8vMiN4uvuKBAkEAWxSAHkvR" 
